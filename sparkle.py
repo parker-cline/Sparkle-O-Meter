f22a947fc9ffefc031e89ac2f65684e88853c363
Parker Cline <parkerpawleysisland@gmail.com>
2021-01-09 15:03:43 -0500

    final changes

--- sparkle.py
+++ sparkle.py
@@ -1,18 +1,15 @@
 from string import punctuation
 deadword = False
+micdrop = False
 badwordlist = [l[0] for l in [word.strip('\n').split(',') for word in open("deadwordlist.txt", 'r').readlines()]]
 deadwordlist = ['good', 'bad', 'what', 'how', 'thing', 'anything', 'something']
 print ("Welcome to the Sparkle-O-Meter -- the definitive program for finetuning your *precise and sophisticated* diction.")
 essay = input("Copy and paste your essay here.").split()
 badwords, x = 0, 0
-for e in essay:
+for e in essay[:-2]:
     essay[x] = e.strip(punctuation).lower()
-    if essay[x] in deadwordlist:
-        print ('0%! YOU HAVE A DEAD WORD. ABORT. ABORT!')
-        deadword = True
-        break
     if essay[x] in badwordlist:
         badwords += 1
+        print(essay[x])
     x += 1
-if not deadword:
-    print ("You scored " + str(100 - (badwords/len(essay)*100)) + "%.")
+print("You scored " + str(100 - (badwords/len(essay)) * 100) + "%.")
